7826055c8a45738beb8cf285a596d198722e645a
Bruno Lopes <bruno.guitarpro@gmail.com>
2023-01-15 23:31:53 -0300

    Initial commit

---
+++ Standups/StandupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandupsApp.swift
+//  Standups
+//
+//  Created by bruno on 15/01/23.
+//
+
+import SwiftUI
+
+@main
+struct StandupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
